8df3bab472dcd84cf59380b4e4a1149ed3ebf729
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2023-09-05 09:01:13 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -17,7 +17,7 @@ class Grupo:
         self.listadoAlumnos = self.listadoAlumnos + lista
 
     def __str__(self):
-         pass
+         return "grupo de estudiantes: " + self._grupo
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
